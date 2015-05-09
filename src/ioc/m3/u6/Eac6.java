48453c4612585dcd4a6d2dac7aee1a98e48fa775
silivia <1996.silvia@gmail.com>
2015-05-09 02:15:15 +0200

    #1 -> inicio del proyecto

---
+++ src/ioc/m3/u6/Eac6.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ioc.m3.u6;
+
+import ioc.eines.Scanner;
+
+/**
+ *
+ * @author josep
+ */
+public class Eac6 {
+    public static void main(String[] args) {
+        Eac6 programa = new Eac6();
+/*
+         * Exemple d'ús de la classe Scanner de ioc.eines per automatitzar el
+         * tecleig de dades. Descomenteu la sentencia per activar-lo
+         */
+        //Scanner.setTestOn("A\n5\n100\n\nb\n\nc\n4\n2014\ns\n");
+        
+        programa.inici();
+         /* 
+         * Exemple d'ús de la classe Scanner de ioc.eines per finalitzar el
+         * tecleig automatitzat de dades. Descomenteu la sentencia per activar-lo
+         */ 
+        //Scanner.setTestOff();        
+    }
+
+    private void inici() {
+        //PER CODIFICAR
+    }
+}
